4738a32f936b1ea17904a620aa45bf587ac65591
Horyzonn <thienphu2221@gmail.com>
2023-07-30 23:48:19 +0700

    repsonsive & js about % toc

--- asset/js/about.js
+++ asset/js/about.js
@@ -1,4 +1,26 @@
 $(document).ready(async()=>{
     initUser()
     initHeaderEvent() 
+    // $(window).scroll(function () { 
+    //     console.log($(this).scrollTop());
+    //     if($(this).scrollTop()>70){
+    //         $('.ads').css({
+    //             'position':'fixed',
+    //             'top':'120px',
+    //         });
+    //     }
+    //     // else if($(this).scrollTop()>$(this).scrollTop()-$('footer').height()){
+    //     //     $('.ads').css({
+    //     //         'position':'abslute',
+    //     //         'bottom': '2rem'
+    //     //     })
+    //     // }
+    //     else{
+    //         $('.ads').css({
+    //             'position':'absolute',
+    //             'top':'2rem'
+    //         });
+    //     }
+    // });
 })
+
